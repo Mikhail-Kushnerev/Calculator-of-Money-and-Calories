6c8cebd502b692a77723cf6da61436f2d33a2d0a
Mikhail <mikushnerev@yandex.ru>
2021-09-30 17:38:43 +0300

    Added PEP8 Standart

--- homework.py
+++ homework.py
@@ -9,7 +9,7 @@ class Calculator:
     def __init__(self, limit):
         self.records = []
         self.limit = limit
-        
+
     def add_record(self, record):
         """Новая запись (число: кол-во калорий или деньги)"""
 
@@ -17,7 +17,7 @@ class Calculator:
 
     def get_today_stats(self):
         """Суточная трата."""
-        
+
         today = (dt.datetime.now()).date()
         today_amount = sum(record.amount for record in self.records
                            if record.date == today)
@@ -38,10 +38,10 @@ class CashCalculator(Calculator):
 
     USD_RATE = 60.0
     EURO_RATE = 70.0
-    
+
     def get_today_cash_remained(self, currency):
         """Подчсёт деняг в валюте."""
-      
+
         money = {
             'rub': ('руб', 1.0),
             'usd': ('USD', self.USD_RATE),
@@ -53,7 +53,7 @@ class CashCalculator(Calculator):
         if currency not in money:
             return 'Некорректное значение'
         if self.get_today_stats() == self.limit:
-            return 'Денег нет, держись'    
+            return 'Денег нет, держись'
         if self.get_today_stats() < self.limit:
             return f'На сегодня осталось {cash} {coin}'
         return f'Денег нет, держись: твой долг - {cash} {coin}'
@@ -61,10 +61,10 @@ class CashCalculator(Calculator):
 
 class CaloriesCalculator(Calculator):
     """Калькулятор калорий."""
-    
+
     def get_calories_remained(self):
         """Остаток калорий"""
-       
+
         remained = self.limit - self.get_today_stats()
         if self.get_today_stats() < self.limit:
             return ('Сегодня можно съесть что-нибудь ещё, '
@@ -75,7 +75,7 @@ class CaloriesCalculator(Calculator):
 
 class Record:
     """Создание записей."""
-    
+
     def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
